adedb632363fe1b5d75b91168afbf81e74bca721
VK-217 <vbandagadde@CSVAVBANDAGADDE.office.comscore.com>
2015-07-23 21:30:36 -0400

    Code indentation

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,8 @@
 ## cacheSolve function takes a matrix as argument and checks if inverse exists and returns it if it does.If the inverse does not exist then it gets the inverse of the matrix using solve function and caches it. 
 ## Write a short comment describing this function
 ## makeCacheMatrix function takes a matrix as argument and contains methods to get/set matrix and get/set inverse of matrices
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) 
+{
 	m <- NULL
 	set <- function(y) 
 	{
@@ -19,17 +20,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-## cacheSolve function takes a matrix as argument and checks if inverse exists and returns it if it does.If the inverse does not exist then it gets the inverse of the matrix using solve function and caches it.
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-		  m<- x$getinverse()
-		  if(!is.null(m))
-		  {
-			message("getting cached matrix")
-			return (m)
-		  }
-		  data <- x$get()
-		  m <- solve(data,...)
-		  x$setinverse(m)
-		  m
+## cacheSolve function takes a matrix as argument and checks if inverse exists and returns it if it does.
+## If the inverse does not exist then it gets the inverse of the matrix using solve function and caches it.
+cacheSolve <- function(x, ...) 
+{
+    ## Return a matrix that is the inverse of 'x'
+	m<- x$getinverse()
+	if(!is.null(m))
+	{
+		message("getting cached matrix")
+		return (m)
+	}
+	data <- x$get()
+	m <- solve(data,...)
+	x$setinverse(m)
+	m
 }
